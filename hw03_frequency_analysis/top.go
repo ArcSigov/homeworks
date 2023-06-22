2e87c400ba8763dc5622fe18e5339a6cb83ff932
AS <arcadii_ru@mail.ru>
2023-06-22 23:06:36 +0300

    HW03 code optimization

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -6,44 +6,34 @@ import (
 	"strings"
 )
 
-type wordinfo struct {
-	word  string
-	count int
-}
-
 var re = regexp.MustCompile(`[^-\pL]`)
 
-func add(character string, sliceStr *[]wordinfo) {
-	for pos := range *sliceStr {
-		if (*sliceStr)[pos].word == character {
-			(*sliceStr)[pos].count++
-			return
-		}
-	}
-	*sliceStr = append(*sliceStr, wordinfo{character, 1})
-}
-
 func Top10(inputStr string) []string {
-	words := strings.Fields(inputStr)
-	sliceStr := make([]wordinfo, 0, len(words))
-	outputStr := make([]string, 0, 10)
-	for _, word := range words {
-		word = strings.ToLower(re.ReplaceAllString(word, ""))
-		if len(word) > 0 && word != "-" {
-			add(word, &sliceStr)
+	inputWords := strings.Fields(inputStr)
+	freqWords := make(map[string]int)
+	for _, word := range inputWords {
+		if word = strings.ToLower(re.ReplaceAllString(word, "")); len(word) > 0 && word != "-" {
+			freqWords[word]++
 		}
 	}
-	sort.Slice(sliceStr, func(i, j int) bool {
-		if sliceStr[i].count == sliceStr[j].count {
-			return sliceStr[i].word < sliceStr[j].word
+
+	words := make([]string, 0, len(freqWords))
+	for k := range freqWords {
+		words = append(words, k)
+	}
+
+	sort.Slice(words, func(i, j int) bool {
+		a := freqWords[words[i]]
+		b := freqWords[words[j]]
+		if a == b {
+			return words[i] < words[j]
 		}
-		return sliceStr[i].count > sliceStr[j].count
+		return a > b
 	})
-	for pos, wordInfo := range sliceStr {
-		if pos == 10 {
-			break
-		}
-		outputStr = append(outputStr, wordInfo.word)
+
+	maxresults := len(words)
+	if maxresults > 10 {
+		maxresults = 10
 	}
-	return outputStr
+	return words[:maxresults]
 }
