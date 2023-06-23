fc42bb80563f287e26bc313523086a5fc65b8501
AS <arcadii_ru@mail.ru>
2023-06-23 23:53:14 +0300

    delete struct and func clear_prev - more optimization:)

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -8,58 +8,50 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
-type characterParam struct {
-	character string
-	isSlash   bool
-}
-
-func checkDigit(current rune, prev *characterParam) (bool, error) {
-	if prev.character == "" {
+func checkDigit(current rune, prev *string, isSlash *bool) (bool, error) {
+	if *prev == "" {
 		return false, ErrInvalidString
-	} else if prev.isSlash {
-		prev.character = string(current)
-		prev.isSlash = false
+	} else if *isSlash {
+		*prev = string(current)
+		*isSlash = false
 		return false, nil
 	}
 	return true, nil
 }
 
-func clearPrev(prev *characterParam) {
-	prev.character = ""
-	prev.isSlash = false
-}
-
 func Unpack(inputStr string) (string, error) {
 	var outputStr strings.Builder
-	var prev characterParam
+	prev := ""
+	isSlash := false
 	for _, current := range inputStr {
 		switch {
 		// if current character is digit -> check digit and write character n-count.
 		case unicode.IsDigit(current):
-			if status, err := checkDigit(current, &prev); errors.Is(err, ErrInvalidString) {
+			if status, err := checkDigit(current, &prev, &isSlash); errors.Is(err, ErrInvalidString) {
 				return "", ErrInvalidString
 			} else if status {
-				outputStr.WriteString(strings.Repeat(prev.character, int(current-'0')))
-				clearPrev(&prev)
+				outputStr.WriteString(strings.Repeat(prev, int(current-'0')))
+				prev = ""
+				isSlash = false
 				continue
 			}
 		// if current character non-digit, write previous or make slashed.
-		case prev.character != "":
+		case prev != "":
 			// make slashed character.
-			if prev.isSlash {
+			if isSlash {
 				if string(current) != `\` {
 					return "", ErrInvalidString
 				}
-				prev.isSlash = false
-				prev.character = string(current)
+				isSlash = false
+				prev = string(current)
 				continue
 			}
-			outputStr.WriteString(prev.character)
+			outputStr.WriteString(prev)
 		}
 		// clear previous and save current in previous.
-		prev.character = string(current)
-		prev.isSlash = string(current) == `\`
+		prev = string(current)
+		isSlash = string(current) == `\`
 	}
-	outputStr.WriteString(prev.character)
+	outputStr.WriteString(prev)
 	return outputStr.String(), nil
 }
