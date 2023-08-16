9973c8603370eb7e020668862a4a782767099833
ASigov <arcadii_ru@mail.ru>
2023-08-16 09:31:06 +0300

    optimize and bug fix

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,32 +9,13 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// task := make(Bi)
-	// out := stages[0](task)
-	// go func() {
-	// 	defer close(task)
-	// 	for res := range in {
-	// 		select {
-	// 		case <-done:
-	// 			return
-	// 		case task <- res:
-	// 		}
-	// 	}
-	// }()
-
-	// for i := 1; i < len(stages); i++ {
-	// 	out = stages[i](out)
-	// }
-	// return out
-
 	out := make(Out)
-	for i := 0; i < len(stages); i++ {
-		i := i
+	for _, stage := range stages {
 		out = func(in In) Out {
 			out := make(Bi)
 			go func() {
 				defer close(out)
-				for result := range stages[i](in) {
+				for result := range in {
 					select {
 					case <-done:
 						return
@@ -43,7 +24,7 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 				}
 			}()
 			return out
-		}(in)
+		}(stage(in))
 		in = out
 	}
 	return out
